d6af2cb25e76a0baa6415103afc5547ce45b4cae
niemiaa-dotcom <niemiaa@gmail.com>
2025-10-06 13:10:03 +0300

    Update fantasy_hockey_optimizer_streamlit.py

--- fantasy_hockey_optimizer_streamlit.py
+++ fantasy_hockey_optimizer_streamlit.py
@@ -1077,8 +1077,11 @@ with tab1:
 
         if st.button("Suorita vertailu"):
             if comparison_type == "Vertaa kahta uutta pelaajaa":
-                if is_valid_player_input(sim_name_A, sim_team_A, sim_positions_A, sim_fpa_A) and \
-                   is_valid_player_input(sim_name_B, sim_team_B, sim_positions_B, sim_fpa_B):
+                if (
+                    is_valid_player_input(sim_name_A, sim_team_A, sim_positions_A, sim_fpa_A)
+                    and is_valid_player_input(sim_name_B, sim_team_B, sim_positions_B, sim_fpa_B)                              
+                ):
+
 
                     
                     original_roster_copy = st.session_state['roster'].copy()
