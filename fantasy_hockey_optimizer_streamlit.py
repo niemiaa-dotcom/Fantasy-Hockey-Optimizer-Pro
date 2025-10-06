8b8df57b265f761add115670c7614af21db5f24c
niemiaa-dotcom <niemiaa@gmail.com>
2025-10-06 13:25:30 +0300

    Update fantasy_hockey_optimizer_streamlit.py

--- fantasy_hockey_optimizer_streamlit.py
+++ fantasy_hockey_optimizer_streamlit.py
@@ -1075,6 +1075,18 @@ with tab1:
                 else:
                     drop_player_fpa = st.number_input("FP/GP", min_value=0.0, step=0.1, format="%.2f", value=0.0, key="drop_player_fpa_empty")
 
+
+        # Pakota syötteet session statesta
+        sim_name_A = st.session_state.get("sim_name_A", "")
+        sim_team_A = st.session_state.get("sim_team_A", "")
+        sim_positions_A = st.session_state.get("sim_positions_A", "")
+        sim_fpa_A = st.session_state.get("sim_fpa_A", 0.0)
+        
+        sim_name_B = st.session_state.get("sim_name_B", "")
+        sim_team_B = st.session_state.get("sim_team_B", "")
+        sim_positions_B = st.session_state.get("sim_positions_B", "")
+        sim_fpa_B = st.session_state.get("sim_fpa_B", 0.0)
+
         if st.button("Suorita vertailu"):
             if comparison_type == "Vertaa kahta uutta pelaajaa":
                 st.write("DEBUG A:", sim_name_A, sim_team_A, sim_positions_A, sim_fpa_A)
