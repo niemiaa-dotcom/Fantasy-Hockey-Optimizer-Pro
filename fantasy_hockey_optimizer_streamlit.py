e167e5a9f49437d60a42aa62fad43d4f7fbc4068
niemiaa-dotcom <niemiaa@gmail.com>
2025-10-09 18:35:30 +0300

    Update fantasy_hockey_optimizer_streamlit.py

--- fantasy_hockey_optimizer_streamlit.py
+++ fantasy_hockey_optimizer_streamlit.py
@@ -295,7 +295,8 @@ if st.sidebar.button("Lataa rosteri Google Sheetsistä", key="roster_button"):
             st.sidebar.error("Rosterin lataaminen epäonnistui. Tarkista Google Sheet -tiedoston sisältö.")
     except Exception as e:
         st.sidebar.error(f"Virhe rosterin lataamisessa: {e}")
-        st.rerun()
+        return pd.DataFrame(), pd.DataFrame()
+
 
 
 # Vapaiden agenttien lataus
