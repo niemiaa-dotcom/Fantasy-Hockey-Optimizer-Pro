8aaa4e67c319631fdcedc91d733963c5864c6cd7
niemiaa-dotcom <niemiaa@gmail.com>
2025-11-30 08:41:17 +0200

    Update fantasy_hockey_optimizer_streamlit.py

--- fantasy_hockey_optimizer_streamlit.py
+++ fantasy_hockey_optimizer_streamlit.py
@@ -7,7 +7,7 @@ import altair as alt
 
 # Page Config
 st.set_page_config(
-    page_title="Fantasy Hockey Analytics (Dynamic)",
+    page_title="Fantasy Hockey Analytics",
     page_icon="🏒",
     layout="wide",
     initial_sidebar_state="expanded"
